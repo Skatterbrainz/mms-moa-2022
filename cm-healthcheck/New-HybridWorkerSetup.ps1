a8b3488a5124daf4fdf361ca8913793dbc1bd80d
skatterbrainz <dstein2017@gmail.com>
2021-12-29 11:56:57 -0500

    12-29 updates

--- cm-healthcheck/New-HybridWorkerSetup.ps1
+++ cm-healthcheck/New-HybridWorkerSetup.ps1
@@ -3,6 +3,8 @@
 	Install and configure Azure Automation Hybrid Worker
 .DESCRIPTION
 	Same as above :)
+.PARAMETER GroupName
+	Name of new hybrid worker group
 .PARAMETER ResourceGroupName
 	Name of Azure Resource Group which contains Automation Account and Log Analytics Workspace
 .PARAMETER AutomationAccountName
@@ -15,6 +17,7 @@
 #>
 [CmdletBinding()]
 param (
+	[parameter(Mandatory=$False)][string]$GroupName = "hw-cm-lab",
 	[parameter(Mandatory=$False)][string]$ResourceGroupName = "rg-cm-lab",
 	[parameter(Mandatory=$False)][string]$AutomationAccountName = "aa-cm-lab",
 	[parameter(Mandatory=$False)][string]$WorkspaceName = "ws-cm-lab"
@@ -40,8 +43,8 @@ try {
 		Write-Host "Script is already installed" -ForegroundColor Green
 	}
 
-	$NewOnPremiseHybridWorkerParameters = @{
-		HybridGroupName       = "hw-cm-lab"
+	$params = @{
+		HybridGroupName       = $GroupName
 		AutomationAccountName = $AutomationAccountName
 		AAResourceGroupName   = $ResourceGroupName
 		OMSResourceGroupName  = $ResourceGroupName
@@ -49,7 +52,12 @@ try {
 		WorkspaceName         = $WorkspaceName
 	}
 	
-	New-OnPremiseHybridWorker.ps1 @NewOnPremiseHybridWorkerParameters
+	if (Get-AzAutomationHybridWorkerGroup -Name $GroupName -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ErrorAction SilentlyContinue) {
+		Write-Host "Creating hybrid worker account" -ForegroundColor Cyan
+		New-OnPremiseHybridWorker.ps1 @params
+	} else {
+		throw "Hybrid worker group not found. Make sure to run New-LabSetup.ps1 first"
+	}
 	
 	Write-Host "Completed successfully!" -ForegroundColor Green
 }
