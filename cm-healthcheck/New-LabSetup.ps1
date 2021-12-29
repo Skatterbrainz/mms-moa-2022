2edc4058c64ada3d7d6fb348c399cc694b13812c
skatterbrainz <dstein2017@gmail.com>
2021-12-29 11:43:32 -0500

    12-29 updates

--- cm-healthcheck/New-LabSetup.ps1
+++ cm-healthcheck/New-LabSetup.ps1
@@ -9,130 +9,150 @@
 	https://github.com/skatterbrainz/mms-moa-2022/cm-healthcheck
 #>
 [CmdletBinding()]
-param()
-
-if (!$azconn) { $azconn = Connect-AzAccount }
-if (!$azconn) { break }
-
-$ResetDemoEnvironment = $False
-$ResourceGroupName = "rg-cm-lab"
-$AutomationAccountName = "aa-cm-lab"
-$Location = "East US"
-if ($Location -eq "East US") {
-	$WSLocation = "East US 2"
-} elseif ($Location -eq "East US 2") {
-	$WSLocation = "East US"
-} else {
-	$WSLocation = $Location
-}
-$WSName = "ws-cm-lab"
-$Tags = @{serviceType="configmgr"; environment="lab"; operation="hybrid"}
-
-# Create Resource Group
-
-$params = @{
-	Name = $ResourceGroupName
-	Location = $Location
-}
-
-if (!(Get-AzResourceGroup @params -ErrorAction SilentlyContinue)) {
-	Write-Host "creating resource group: $($params.Name)" -ForegroundColor Cyan
-	New-AzResourceGroup @params
-} else {
-	Write-Host "resource group exists: $($params.Name)" -ForegroundColor Green
-	if ($ResetDemoEnvironment -eq $True) {
-		Write-Warning "removing entire demo lab!"
-		Get-AzResourceGroup @params | Remove-AzResourceGroup -Force 
-		Write-Host "allow time for resources to be removed before running setup again" -ForegroundColor Yellow
-		break
-	}
-}
-
-# Create Automation Account
-
-$params = @{
-	Name = $AutomationAccountName
-	ResourceGroupName = $ResourceGroupName
-	Location = $Location
-	AssignSystemIdentity = $True
-	Tags = $Tags
-}
-
-if (!(Get-AzAutomationAccount -Name $params.Name -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue)) {
-	Write-host "creating automation account: $($params.Name)" -ForegroundColor Cyan
-	New-AzAutomationAccount @params
-} else {
-	Write-host "automation account exists: $($params.Name)" -ForegroundColor Green
-}
-
-# Create Log Analytics Workspace
-
-$params = @{
-	ResourceGroupName = $ResourceGroupName
-	Name = $WSName
-	Location = $WSLocation
-}
-
-if (!(Get-AzOperationalInsightsWorkspace -Name $params.Name -ResourceGroupName $params.ResourceGroupName -ErrorAction SilentlyContinue)) {
-	Write-host "creating workspace: $($params.Name)" -ForegroundColor Cyan
-	New-AzOperationalInsightsWorkspace @params
-} else {
-	Write-host "workspace exists: $($params.Name)" -ForegroundColor Green
-}
-
-# Create Automation Account Variables
-
-$params = @{
-	Name = "LastHealthResult"
-	Encrypted = $False
-	Value = "PENDING"
-	AutomationAccountName = $AutomationAccountName
-	ResourceGroupName = $ResourceGroupName
-}
-
-if (!(Get-AzAutomationVariable -Name $params.Name -ResourceGroupName $params.ResourceGroupName -AutomationAccountName $params.AutomationAccountName -ErrorAction SilentlyContinue)) {
-	Write-host "creating variable: $($params.Name)" -ForegroundColor Cyan
-	New-AzAutomationVariable @params
-} else {
-	Write-Host "variable exists: $($params.Name)" -ForegroundColor Green
-}
-
-$params = @{
-	Name = "LastHealthCheck"
-	Encrypted = $False
-	Value = "PENDING"
-	AutomationAccountName = $AutomationAccountName
-	ResourceGroupName = $ResourceGroupName
-}
-
-if (!(Get-AzAutomationVariable -Name $params.Name -ResourceGroupName $params.ResourceGroupName -AutomationAccountName $params.AutomationAccountName -ErrorAction SilentlyContinue)) {
-	Write-host "creating variable: $($params.Name)" -ForegroundColor Cyan
-	New-AzAutomationVariable @params
-} else {
-	Write-Host "variable exists: $($params.Name)" -ForegroundColor Green
-}
-
-# Create (Import) Runbook
-
-$params = @{
-	Path = ".\Runbook-InvokeCmHealthCheck.ps1"
-	ResourceGroup = $ResourceGroupName
-	AutomationAccountName = $AutomationAccountName
-	Type = "PowerShell"
-	Description = "Run ConfigMgr Health Check"
-	Tags = $Tags
-	Published = $True
-}
+param (
+	[parameter(Mandatory=$False)][string]$ResourceGroupName = "rg-cm-lab",
+	[parameter(Mandatory=$False)][string]$AutomationAccountName = "aa-cm-lab",
+	[parameter(Mandatory=$False)][string]$WorkspaceName = "ws-cm-lab",
+	[parameter(Mandatory=$False)][string]$Location = "East US",
+	[parameter(Mandatory=$False)][string]$Tags = "@{ServiceType='ConfigMgr';Environment='Lab';Operation='Hybrid'}",
+	[parameter(Mandatory=$False)][switch]$ResetDemoEnvironment
+)
 
 try {
-	$runbookName = $(Get-Item $params.Path -ErrorAction Stop | Select-Object -ExpandProperty BaseName)
-	if (!(Get-AzAutomationRunbook -Name $runbookName -AutomationAccountName $AutomationAccountName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue)) {
-		Write-host "importing runbook: $runbookName" -ForegroundColor Cyan
-		Import-AzAutomationRunbook @params
-	} else {
-		Write-Host "runbook exists: $runbookName" -ForegroundColor Green
+	if (-not $azconn) { $azconn = Connect-AzAccount }
+	if (-not $azconn) { throw "authentication not completed" }
+
+	if (-not [string]::IsNullOrEmpty($Tags)) {
+		$TagSet = Invoke-Expression $Tags
 	}
-	$go = $true
+	# Create Resource Group
+
+	$params = @{
+		Name = $ResourceGroupName
+		Location = $Location
+		Tag = $TagSet
+	}
+
+	if (-not (Get-AzResourceGroup @params -ErrorAction SilentlyContinue)) {
+		Write-Host "creating resource group: $($params.Name)" -ForegroundColor Cyan
+		New-AzResourceGroup @params
+	} else {
+		Write-Host "resource group exists: $($params.Name)" -ForegroundColor Green
+		if ($ResetDemoEnvironment -eq $True) {
+			Write-Warning "removing entire demo lab!"
+			Get-AzResourceGroup @params | Remove-AzResourceGroup -Force 
+			Write-Host "allow time for resources to be removed before running setup again" -ForegroundColor Yellow
+			break
+		}
+	}
+
+	# Create Automation Account
+
+	$params = @{
+		Name = $AutomationAccountName
+		ResourceGroupName = $ResourceGroupName
+		Location = $Location
+		AssignSystemIdentity = $True
+		Tags = $TagSet
+	}
+
+	if (-not (Get-AzAutomationAccount -Name $params.Name -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue)) {
+		Write-host "creating automation account: $($params.Name)" -ForegroundColor Cyan
+		New-AzAutomationAccount @params
+	} else {
+		Write-host "automation account exists: $($params.Name)" -ForegroundColor Green
+	}
+
+	# Create Log Analytics Workspace
+
+	if ($Location -eq "East US") {
+		$WSLocation = "East US 2"
+	} elseif ($Location -eq "East US 2") {
+		$WSLocation = "East US"
+	} else {
+		$WSLocation = $Location
+	}
+
+	$params = @{
+		ResourceGroupName = $ResourceGroupName
+		Name = $WorkspaceName
+		Location = $WSLocation
+		Tag = $TagSet
+	}
+
+	if (-not (Get-AzOperationalInsightsWorkspace -Name $params.Name -ResourceGroupName $params.ResourceGroupName -ErrorAction SilentlyContinue)) {
+		Write-host "creating workspace: $($params.Name)" -ForegroundColor Cyan
+		New-AzOperationalInsightsWorkspace @params
+	} else {
+		Write-host "workspace exists: $($params.Name)" -ForegroundColor Green
+	}
+
+	# Create Automation Account Variables
+
+	$params = @{
+		Name = "LastHealthResult"
+		Encrypted = $False
+		Value = "PENDING"
+		AutomationAccountName = $AutomationAccountName
+		ResourceGroupName = $ResourceGroupName
+	}
+
+	if (-not (Get-AzAutomationVariable -Name $params.Name -ResourceGroupName $params.ResourceGroupName -AutomationAccountName $params.AutomationAccountName -ErrorAction SilentlyContinue)) {
+		Write-host "creating variable: $($params.Name)" -ForegroundColor Cyan
+		New-AzAutomationVariable @params
+	} else {
+		Write-Host "variable exists: $($params.Name)" -ForegroundColor Green
+	}
+
+	$params = @{
+		Name = "LastHealthCheck"
+		Encrypted = $False
+		Value = "PENDING"
+		AutomationAccountName = $AutomationAccountName
+		ResourceGroupName = $ResourceGroupName
+	}
+
+	if (-not (Get-AzAutomationVariable -Name $params.Name -ResourceGroupName $params.ResourceGroupName -AutomationAccountName $params.AutomationAccountName -ErrorAction SilentlyContinue)) {
+		Write-host "creating variable: $($params.Name)" -ForegroundColor Cyan
+		New-AzAutomationVariable @params
+	} else {
+		Write-Host "variable exists: $($params.Name)" -ForegroundColor Green
+	}
+
+	$runbooks = ("Runbook-InvokeCmHealthCheck.ps1","Runbook-TestHybridWorker.ps1")
+	foreach ($runbook in $runbooks) {
+		$params = @{
+			Path = ".\$($runbook)"
+			ResourceGroup = $ResourceGroupName
+			AutomationAccountName = $AutomationAccountName
+			Type = "PowerShell"
+			Tags = $Tags
+			Published = $True
+		}
+		$runbookName = $(Get-Item $params.Path -ErrorAction Stop | Select-Object -ExpandProperty BaseName)	
+		if (-not (Get-AzAutomationRunbook -Name $runbookName -AutomationAccountName $AutomationAccountName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue)) {
+			Write-host "importing runbook: $runbookName" -ForegroundColor Cyan
+			Import-AzAutomationRunbook @params
+		} else {
+			Write-Host "runbook exists: $runbookName" -ForegroundColor Green
+		}
+	}
+	
+	$params = @{
+		Name = "hw-cm-lab"
+		ResourceGroupName = $ResourceGroupName
+		AutomationAccountName = $AutomationAccountName
+	}
+	
+	if (-not (Get-AzAutomationHybridWorkerGroup @params -ErrorAction SilentlyContinue)) {
+		Write-Host "creating hw group: $($params.Name)" -ForegroundColor Cyan
+		$hwg = New-AzAutomationHybridWorkerGroup @params
+		$hwg | select *
+	} else {
+		Write-Host "hw group exists: $($params.Name)" -ForegroundColor Green
+	}
+	
+	Write-Host "Azure lab environment setup completed!" -ForegroundColor Green
 }
 catch {
 	if ($_.Exception.Message -like "Cannot find path*") {
@@ -141,30 +161,3 @@ catch {
 		Write-Error $_.Exception.Message
 	}
 }
-
-if (!$go) { break }
-# Create Hybrid Worker Group
-
-$params = @{
-	Name = "hw-cm-lab"
-	ResourceGroupName = $ResourceGroupName
-	AutomationAccountName = $AutomationAccountName
-}
-
-if (!(Get-AzAutomationHybridWorkerGroup @params -ErrorAction SilentlyContinue)) {
-	Write-Host "creating hw group: $($params.Name)" -ForegroundColor Cyan
-	$hwg = New-AzAutomationHybridWorkerGroup @params
-	$hwg | select *
-} else {
-	Write-Host "hw group exists: $($params.Name)" -ForegroundColor Green
-}
-
-<#
-$params = @{
-	ResourceGroupName = $ResourceGroupName 
-	WorkspaceName = $WSName 
-	IntelligencePackName = "AzureAutomation" 
-	Enabled = $true
-}
-Set-AzOperationalInsightsIntelligencePack @params
-#>
--- cm-healthcheck/README.md
+++ cm-healthcheck/README.md
@@ -1,15 +1,39 @@
-# Session: Automate ConfigMgr Health Checks with Azure Automation
+# MMS MOA 2022 Session
+
+## Title: Automate ConfigMgr Health Checks with Azure Automation
 
 ## Overview
 
+TBD
+
 ## Setup
 
-* Copy script files to a common location
-* Install prereq modules:
-  * Az
-  * Az.Accounts
-  * Az.Automation
-  * Az.OperationalInsights
-* Run New-LabSetup.ps1 to create Azure environment
-* Run New-HybridWorkerSetup.ps1 on lab VM
-  * Install module on lab VM: cmhealth
+* Azure Subscription (pay-as-you-go or higher)
+
+### Azure Setup
+
+1. Copy *.ps1 files to a common location on local computer
+2. Open PowerShell console as Administrator
+3. Edit/Save New-LabSetup.ps1 to suit your environment
+4. Run script New-LabSetup.ps1
+
+### ConfigMgr Setup
+
+1. Log onto ConfigMgr primary server as administrator
+2. Copy _New-HybridWorkerSetup.ps1_ to local computer
+3. Open PowerShell console as Administrator
+4. Edit/Save _New-HybridWorkerSetup.ps1_ to suit your environment
+5. Run _New-HybridWorkerSetup.ps1_
+6. Install the [cmhealth](https://powershellgallery.com/packages/cmhealth/) PowerShell module
+
+### Test Runbook
+
+1. Log into the Azure portal
+2. Navigate to __Automation Accounts__ / __aa-cm-lab__
+3. Select __Runbooks__
+4. Select runbook __Runbook-TestHybridWorker__
+5. Click __Start__
+6. Select Run on = _Hybrid Worker_
+7. Make sure _hw-cm-lab_ is selected, and click __OK__
+8. When Status changes to "Completed" select "Output"
+9. Verify the NetBIOS hostname is displayed (e.g. "CM01")
